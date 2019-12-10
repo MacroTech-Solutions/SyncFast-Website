ed0daaad60f826750652dfc51d19da85bda1bd85
JoshRak <48808816+JoshRak@users.noreply.github.com>
2019-12-10 11:36:38 -0500

    Moved create to aws

--- docs/lib/js/createScripts.js
+++ docs/lib/js/createScripts.js
@@ -2,8 +2,6 @@ if (sessionStorage.getItem('userKey') == null || sessionStorage.getItem('userKey
   window.location.href = "login.html";
 }
 
-const database = firebase.database().ref();
-
 let SpeechRecognition = window.webkitSpeechRecognition;
 let recognition = new SpeechRecognition();
 let textDiv = document.getElementById('bullets');
@@ -39,9 +37,9 @@ async function setup() {
   })
     .then(data => result = data.data)
     .catch(err => console.log(err))
-    sessionStorage.setItem('accessKey', );
-    sessionStorage.setItem('firebasePresentationKey', result.firebasepresentationkey)
-    sessionStorage.setItem('currentSlide', );
+  sessionStorage.setItem('accessKey', result.accesskey);
+  sessionStorage.setItem('firebasePresentationKey', result.firebasepresentationkey)
+  sessionStorage.setItem('currentSlide', result.currentslide);
 }
 
 recognition.start();
@@ -104,7 +102,15 @@ recognition.onresult = async function (event) {
     secondStop = false;
     words = lastInput.split(" ");
     currentSlideTitle = capitalizeFirstLetter(words[words.length - 1]);
-    firebase.database().ref(`presentations/${sessionStorage.getItem('firebasePresentationKey')}/presentationTitle`).set(currentSlideTitle);
+    axios({
+      method: 'POST',
+      url: 'https://cors-anywhere.herokuapp.com/https://syncfastserver.macrotechsolutions.us/createTitle',
+      headers: {
+        'Content-Type': 'application/json',
+        'slidetitle': currentSlideTitle,
+        'firebasepresentationkey': sessionStorage.getItem('firebasePresentationKey')
+      }
+    });
     document.getElementById('heading').innerText = randomBeginning() + capitalizeFirstLetter(words[words.length - 1]);
     document.getElementById('heading').style.backgroundColor = `rgb(${Math.random() * 101 + 50},${Math.random() * 101 + 50},${Math.random() * 101 + 50})`;
   } else if (!firstSentence && !secondStop) {
@@ -125,10 +131,26 @@ recognition.onresult = async function (event) {
       });
       var1 = myCanvas.toDataURL("image/png");
       console.log(var1);
-      firebase.database().ref(`presentations/${sessionStorage.getItem('firebasePresentationKey')}/slideUrl`).set(var1);
+      axios({
+        method: 'POST',
+        url: 'https://cors-anywhere.herokuapp.com/https://syncfastserver.macrotechsolutions.us/slideUrl',
+        headers: {
+          'Content-Type': 'application/json',
+          'slideurl': var1,
+          'firebasepresentationkey': sessionStorage.getItem('firebasePresentationKey')
+        }
+      });
       let imageUrl = document.getElementById("image").src;
 
-      firebase.database().ref(`presentations/${sessionStorage.getItem('firebasePresentationKey')}/imageUrl`).set(imageUrl);
+      axios({
+        method: 'POST',
+        url: 'https://cors-anywhere.herokuapp.com/https://syncfastserver.macrotechsolutions.us/imageUrl',
+        headers: {
+          'Content-Type': 'application/json',
+          'imageurl': imageUrl,
+          'firebasepresentationkey': sessionStorage.getItem('firebasePresentationKey')
+        }
+      });
     } else {
       if (state == false) {
         document.getElementById(`bullet${bulletNum}`).style.display = "block";
@@ -210,7 +232,14 @@ async function checkInput(input) {
       bulletNum--;
     }
     else if (input.includes("end") && input.includes("slideshow") || input.includes("end") && input.includes("presentation") || input.includes("finish") && input.includes("presentation") || input.includes("finish") && input.includes("slideshow")) {
-      firebase.database().ref(`presentations/${sessionStorage.getItem('firebasePresentationKey')}/slideUrl`).set("assets/SyncFast_End_Screen.png");
+      axios({
+        method: 'POST',
+        url: 'https://cors-anywhere.herokuapp.com/https://syncfastserver.macrotechsolutions.us/endScreen',
+        headers: {
+          'Content-Type': 'application/json',
+          'firebasepresentationkey': sessionStorage.getItem('firebasePresentationKey')
+        }
+      });
       //send endscreen.png to all viewers
       window.location.href = "index.html";
     }
