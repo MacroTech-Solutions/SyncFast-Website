cc888116157f9938dd22fff2b83b9f8153ff7069
aryatschand <43081871+aryatschand@users.noreply.github.com>
2019-12-17 10:01:29 -0500

    qr location change

--- docs/lib/js/slidesPresentScripts.js
+++ docs/lib/js/slidesPresentScripts.js
@@ -105,7 +105,7 @@ async function listSlides() {
         gapi.client.slides.presentations.pages.getThumbnail({
             presentationId: sessionStorage.getItem('presentationID'),
             pageObjectId: presentation.slides[sessionStorage.getItem('currentSlide')].objectId,
-        }).then(function (response) {
+        }).then(async function (response) {
             const res = JSON.parse(response.body);
             slideUrl = res.contentUrl;
             firebase.database().ref(`presentations/${sessionStorage.getItem('firebasePresentationKey')}/slideUrl`).set(slideUrl);
@@ -127,10 +127,9 @@ async function listSlides() {
             })
                 .then(data => result = data.data)
                 .catch(err => console.log(err))
-                console.log("hello")
-                console.log(result)
-                console.log("arya")
-            document.querySelector(".center").prepend(p);
+                let qrElement = result;
+                document.querySelector(".img").appendChild(qrElement);
+                document.querySelector(".center").prepend(p);
         }, function (response) {
             console.log('Error: ' + response.result.error.message);
         });
